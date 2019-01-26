deaed0c81c7e49352f62a703a1e4ec9b63e1d03d
huteko <htooko1101@gmail.com>
2019-01-26 23:33:55 +0630

    add spinner at the middle of toolbar add StackLayoutManager add Button at the bottom

--- app/src/main/java/io/htueko/comeacrossfood/MainActivity.kt
+++ app/src/main/java/io/htueko/comeacrossfood/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.OrientationHelper
+import android.view.Menu
+import com.littlemango.stacklayoutmanager.StackLayoutManager
 import io.htueko.comeacrossfood.adapter.BottomAdapter
 import io.htueko.comeacrossfood.adapter.TopAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,23 +18,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar_main)
 
-        val header : ArrayList<String> = ArrayList()
-        for (i in 1..10){
+        val header: ArrayList<String> = ArrayList()
+        for (i in 1..10) {
             header.add("Header Text $i")
         }
 
-        val image : ArrayList<Int> = ArrayList()
-        image.add(R.drawable.ic_smiley_05)
-        image.add(R.drawable.ic_smiley_04)
-        image.add(R.drawable.ic_smiley_03)
-        image.add(R.drawable.ic_smiley_02)
-        image.add(R.drawable.ic_smiley_01)
+        val image: ArrayList<Int> = ArrayList()
+        image.add(R.drawable.apple)
+        image.add(R.drawable.code_burger)
+        image.add(R.drawable.coffee)
+        image.add(R.drawable.fish)
+        image.add(R.drawable.grape)
 
-        recycler_view_main_bottom.layoutManager = LinearLayoutManager(this, OrientationHelper.HORIZONTAL, false)
+        val orientation = StackLayoutManager.ScrollOrientation.RIGHT_TO_LEFT
+        val stackLayoutManager = StackLayoutManager(orientation)
+        recycler_view_main_bottom.layoutManager = stackLayoutManager
         recycler_view_main_bottom.adapter = BottomAdapter(header)
 
         recycler_view_main_top.layoutManager = LinearLayoutManager(this, OrientationHelper.HORIZONTAL, false)
         recycler_view_main_top.adapter = TopAdapter(image)
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
--- app/src/main/java/io/htueko/comeacrossfood/adapter/TopAdapter.kt
+++ app/src/main/java/io/htueko/comeacrossfood/adapter/TopAdapter.kt
@@ -4,12 +4,16 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import de.hdodenhof.circleimageview.CircleImageView
 import io.htueko.comeacrossfood.R
 
 class TopAdapter(private val image: ArrayList<Int>) : RecyclerView.Adapter<TopAdapter.MyViewHolder>(){
+
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): MyViewHolder {
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.list_card_top, parent, false)
+        val rotate = AnimationUtils.loadAnimation(parent.context, R.anim.rotate_anim)
+        view.startAnimation(rotate)
         return MyViewHolder(view)
     }
 
@@ -20,7 +24,6 @@ class TopAdapter(private val image: ArrayList<Int>) : RecyclerView.Adapter<TopAd
     override fun getItemCount(): Int {
         return image.size
     }
-
 
     class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
         val image: CircleImageView = itemView.findViewById(R.id.imgView_card_top)
